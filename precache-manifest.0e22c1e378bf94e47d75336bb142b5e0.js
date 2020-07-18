e0439857dc0935fb96cad6c313ef018cf505c0d0
Mark Gitau 23 <markgitau@flume.cs.dartmouth.edu>
2020-07-18 18:23:25 -0400

    Updates

---
+++ precache-manifest.0e22c1e378bf94e47d75336bb142b5e0.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c25b3992b418b27342c827795fc3c0",
+    "url": "/kadari/index.html"
+  },
+  {
+    "revision": "97cf1a1bf24255f1b231",
+    "url": "/kadari/static/css/main.a9e923cc.chunk.css"
+  },
+  {
+    "revision": "63e19825cfb0ae67c0b2",
+    "url": "/kadari/static/js/2.89a43c02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kadari/static/js/2.89a43c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cf1a1bf24255f1b231",
+    "url": "/kadari/static/js/main.56245878.chunk.js"
+  },
+  {
+    "revision": "dae23637a76432a76f7b",
+    "url": "/kadari/static/js/runtime-main.b637e1e7.js"
+  },
+  {
+    "revision": "9bfb53762791ddb15748d074b1853b49",
+    "url": "/kadari/static/media/IMG_9150-compressed.9bfb5376.CR2"
+  },
+  {
+    "revision": "ed490f5175b50a91ff2dddd4a37a0a8e",
+    "url": "/kadari/static/media/amani-ring.ed490f51.PNG"
+  },
+  {
+    "revision": "47aca2ca679f0353cf6646c23d933438",
+    "url": "/kadari/static/media/duara-earrings.47aca2ca.jpeg"
+  },
+  {
+    "revision": "3653b3e2b388a8e4c020a44ae54689df",
+    "url": "/kadari/static/media/fahari-earrings.3653b3e2.PNG"
+  },
+  {
+    "revision": "1ba8fca4be078d8d1c1c8395138d2f85",
+    "url": "/kadari/static/media/giriama-pendant-necklace.1ba8fca4.PNG"
+  },
+  {
+    "revision": "5f09d6629e74032efd71bc43263f6d6e",
+    "url": "/kadari/static/media/kadee-earrings.5f09d662.PNG"
+  },
+  {
+    "revision": "4760811631fead01a463b284c4e3f848",
+    "url": "/kadari/static/media/kambe-bracelet.47608116.PNG"
+  },
+  {
+    "revision": "4e61889201d9f84b7b47e312cbd7f1fe",
+    "url": "/kadari/static/media/kasandi-bracelet.4e618892.PNG"
+  },
+  {
+    "revision": "91d6808b08a6697fd68598d62efac118",
+    "url": "/kadari/static/media/kumi-kumi-necklace.91d6808b.PNG"
+  },
+  {
+    "revision": "81828d2ecbc4f2680adb343b9182befd",
+    "url": "/kadari/static/media/lazi-earrings.81828d2e.PNG"
+  },
+  {
+    "revision": "bcf6ca1a0021904375c5e9d6a528dad1",
+    "url": "/kadari/static/media/libra-earrings.bcf6ca1a.PNG"
+  },
+  {
+    "revision": "617806ec6e5ae4867c6daceb617c68e3",
+    "url": "/kadari/static/media/matatu-earrings.617806ec.PNG"
+  },
+  {
+    "revision": "fdeacf4ab660df0eeec3b922fc15ec6e",
+    "url": "/kadari/static/media/mueni-bracelet.fdeacf4a.PNG"
+  },
+  {
+    "revision": "d38124ab6915650651fa461a80c87a11",
+    "url": "/kadari/static/media/nafula-ring.d38124ab.PNG"
+  },
+  {
+    "revision": "6780ee83ec5d76d09289eeac355dfe1a",
+    "url": "/kadari/static/media/nami-earrings.6780ee83.jpeg"
+  },
+  {
+    "revision": "45e625ce0e1bf6cb324db5ce9c55e3d0",
+    "url": "/kadari/static/media/njoki-studs.45e625ce.png"
+  },
+  {
+    "revision": "50e9d960ee96a02ce51d2d551897535f",
+    "url": "/kadari/static/media/pendo-stud-earrings.50e9d960.PNG"
+  },
+  {
+    "revision": "33f1564223576ddfa202a3a27816f9a4",
+    "url": "/kadari/static/media/sifa-earrings.33f15642.PNG"
+  },
+  {
+    "revision": "06a854481ed5a1f9acabcb6624eb91a1",
+    "url": "/kadari/static/media/simba-earrings.06a85448.PNG"
+  },
+  {
+    "revision": "7b8e0849aedf54c8e0666d2955d544fb",
+    "url": "/kadari/static/media/tachonyi-set.7b8e0849.jpeg"
+  },
+  {
+    "revision": "f1a45876947ce51a661cd6c7dd85c029",
+    "url": "/kadari/static/media/tawi-earrings.f1a45876.png"
+  },
+  {
+    "revision": "38be71154647bbecd53d79889084ddd8",
+    "url": "/kadari/static/media/tumaini-earrings.38be7115.PNG"
+  },
+  {
+    "revision": "797ddf1e733f13e3df9c4a81704334f3",
+    "url": "/kadari/static/media/ukariri-mantra-bracelet.797ddf1e.jpg"
+  },
+  {
+    "revision": "1b3736a315caf1fb4b4bb30296a19d8c",
+    "url": "/kadari/static/media/wanja-bracelet.1b3736a3.PNG"
+  },
+  {
+    "revision": "704abce160e663748cbb17c311460c22",
+    "url": "/kadari/static/media/zuri-bracelet.704abce1.jpeg"
+  }
+]);
